e15e05b161fafe2f1d841594de0933ec6760f3be
Akanksha3231 <104375620+Akanksha3231@users.noreply.github.com>
2022-04-26 17:19:29 +0530

    Add files via upload

---
+++ Naturals.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#bars');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new swiper('.swiper-container',{
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true ,
+        renderBullet: function(index, className) {
+            return '<span class="'  + className + '">' + (index + i) + '</span>';
+        },
+    },
+});
+
+document.querySelectorAll('.top-image').forEach(image_1=>{
+    image_1.addEventListener('click',() =>{
+        var src = image_1.getAttribute('src');
+        document.querySelector('.big-image1').src = src;
+    });
+});
